3f871b522d4ad668fab00e8bd35a2daa322f366a
Chelariu Alexandru <alexchelariu834@gmail.com>
2026-08-19 16:23:48 +0300

    Added C++ guards inside database header

--- logic/database/include/database_header.h
+++ logic/database/include/database_header.h
@@ -4,47 +4,54 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef struct {
-    int sock;
-    char status;
-    int connected;
-} DatabaseConnection;
+#ifdef __cplusplus
+extern "C" {
+    #endif
+    typedef struct {
+        int sock;
+        char status;
+        int connected;
+    } DatabaseConnection;
 
-typedef struct {
-    char *data;
-    int length;
-} DatabaseValue;
+    typedef struct {
+        char *data;
+        int length;
+    } DatabaseValue;
 
-typedef struct {
-    char *name;
-    int type_oid;
-} DatabaseColumn;
+    typedef struct {
+        char *name;
+        int type_oid;
+    } DatabaseColumn;
 
-typedef struct {
-    int status;
+    typedef struct {
+        int status;
 
-    int number_columns;
-    DatabaseColumn *columns;
+        int number_columns;
+        DatabaseColumn *columns;
 
-    int number_rows;
-    int row_capacity;
-    DatabaseValue **rows;
+        int number_rows;
+        int row_capacity;
+        DatabaseValue **rows;
 
-    char *error;
-} DatabaseResult;
+        char *error;
+    } DatabaseResult;
 
-DatabaseConnection *db_connect(const char *config_path, const char *password);
+    DatabaseConnection *db_connect(const char *config_path, const char *password);
 
-DatabaseResult *db_query(DatabaseConnection *conn, const char *sql);
-DatabaseResult *database_query(DatabaseConnection *conn, const char *sql);
-DatabaseResult *database_query_params(int sock, const char *sql, const char **params, int nparams);
+    DatabaseResult *db_query(DatabaseConnection *conn, const char *sql);
+    DatabaseResult *database_query(DatabaseConnection *conn, const char *sql);
+    DatabaseResult *database_query_params(int sock, const char *sql, const char **params, int nparams);
 
-void database_result_free(DatabaseResult *res);
+    void database_result_free(DatabaseResult *res);
+    void db_disconnect(DatabaseConnection *conn);
 
-const char *database_get_value(const DatabaseResult *res, int row, int col);
+    const char *database_get_value(const DatabaseResult *res, int row, int col);
 
-int database_is_null(const DatabaseResult *res, int row, int col);
-int wait_for_read(int sock);
+    int database_is_null(const DatabaseResult *res, int row, int col);
+    int wait_for_read(int sock);
 
+    #ifdef __cplusplus
+}
+#endif
 
 #endif
